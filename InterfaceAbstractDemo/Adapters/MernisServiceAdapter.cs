0789937912147af9196f6344d66d84e51e0140d2
BurakGenc <61274510+BurakGenc@users.noreply.github.com>
2021-03-19 22:54:46 +0300

    first commit

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+   public class MernisServiceAdepter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+            return client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName,customer.LastName,customer.DateOfBirth.Year).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
+
+
+
+
+ 
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Concrete;
+using System;
+using MernisServiceReference;
+using InterfaceAbstractDemo.Adapters;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdepter()); ;
+            customerManager.Save(new Customer {DateOfBirth=new DateTime(1998,1,24),FirstName="Burak",Id=1,LastName="Genç",NationalityId="23104176914" });
+            Console.ReadLine();   
+        }
+    }
+}
